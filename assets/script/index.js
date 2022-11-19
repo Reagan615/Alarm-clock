639a70e2cc2a08d322361998362191be675aba78
Reagan615 <419247451@qq.com>
2022-11-18 19:30:15 -0600

    modify js

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,23 +1,8 @@
 'use strict';
-/* let links = document.querySelectorAll(' nav a'),
-
-function setActive(el) {
-links.forEach(link => {
-link.classList.remove('active')
-});
-el.classList.add('active'); 
-};
-
-
-links.forEach((link,index) => {
-link.addEventListener('click', e => {
-e.preventDefault();
-bg.style.transform = `translate(${125*index}px,-50%)`;
-setActive(e.currentTarget);     
-})  
-}) ; */
 
 let showTime = document.querySelector('.show-time');
+let show = new Date();
+
 setTimeout(time);
 let setTime = setTimeout(time, 1000);
 function time() {
@@ -33,35 +18,46 @@ let writeAlarm1 = document.querySelector('.set-time .hour');
 let writeAlarm2 = document.querySelector('.set-time .minute');
 let setAlarm = document.querySelector('.set-alarm button');
 let showAlarm = document.querySelector('.alarm-time');
+let icon = document.querySelector('.icon');
 let tips = document.querySelector('.tips');
 const alarm = new Audio('C:/Users/lenovo/Desktop/Alarm clock/assets/audio/alarm.mp3');
 alarm.type = 'audio/mp3';
 
-
 setAlarm.addEventListener('click', function() {
-    let a = writeAlarm1.value;
-    let b = writeAlarm2.value;
-    let show = new Date();
-    let h = show.getHours().toString();
-    let m = show.getMinutes().toString();
+   let a = writeAlarm1.value;
+   let b = writeAlarm2.value;
+
     if (a == '' || b == '') {
         tips.style.visibility = 'visible';
-    } if (a > 0 && a < 24 && b > 0 && b < 60) {
+    } if (a > 0 && a < 24 && b >= 0 && b < 60) {
         showAlarm.innerText = `${a} : ${b}`;
         showAlarm.style.visibility = 'visible';
+        icon.classList.add('icon2');
     }else {
         tips.style.visibility = 'visible';
-    }
-    console.log(a);
-    console.log(b);
-    console.log(h);
-    console.log(m);
+    };
+    setInterval(function() {
+        let show = new Date();
+        let h = show.getHours().toString().padStart(2, '0');
+        let m = show.getMinutes().toString().padStart(2, '0');
+       /*  console.log(a);
+        console.log(b);
+        console.log(h);
+        console.log(m); */
+        if(a === h && b === m) {
+            showTime.style.removeProperty('color');
+            showTime.classList.add('change');
+            alarm.play();
+          };
+    },1000)
 
-    if(a === h && m === b) {
-        showTime.style.color = 'red';
-        alarm.play();
-    }
 });
+
+
+
+
+
+
 
 
 
